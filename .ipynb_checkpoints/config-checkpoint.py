0ae6137ba726e5517a083a13e59fe189f9bcd27a
Kaylee Freund <kbeth91@gmail.com>
2020-03-15 15:15:34 -0400

    Finished weatherpy

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,34 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "api_key = \"7f81cbf0da994f0cf31e7c7b6d8a0a5c\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
---
+++ config.py
@@ -0,0 +1,1 @@
+api_key = "7f81cbf0da994f0cf31e7c7b6d8a0a5c"
